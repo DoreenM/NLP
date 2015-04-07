ec3fa4d5400afb12d6ea4144e970a1ac07fe2145
Doreen Mokeira <doreenmokeira@yahoo.com>
2015-04-07 16:07:08 +0300

    Create

--- src/main/java/KMeansClusterer.java
+++ src/main/java/KMeansClusterer.java
@@ -3,12 +3,22 @@
  */
 public class KMeansClusterer {
 
+    private static void initializeClassVariable() {
+        System.out.println("Centroids initialized at:");
+        String POSITIVE;
+        String NEGATIVE;
+        String NEUTRAL;
+
+    
+    }
+
 
         public static final int CLUSTERS = 3;
 
         public static void main(String[] args) {
 
             String data =MyGUIProgram.mytext.getText();
+
 
         }
 
